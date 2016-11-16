ce7a4bed6d31aae4f54cceda3d224950143e0677
Samantha <samsachow@gmail.com>
2016-11-16 15:23:27 -0800

    Beginning of November
    FracCalc

--- FracCalc1/src/FracCalcCheckpoint1.java
+++ FracCalc1/src/FracCalcCheckpoint1.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
 
 public class FracCalcCheckpoint1 {
 
+	public static void main(String[] args){
+		System.out.println("Enter statement: ");
+		Scanner userInput = new Scanner(System.in);
+		String statement = userInput.nextLine();
+		
+		System.out.println(produceAnswer(statement));
+	}
+
+	public static String produceAnswer(String statement){
+		String [] statmentParts = statement.split(" ");
+		String firstOperand = statementParts[0];
+		String operator = statementParts[1];
+		String secondOperand = statementParts[2];
+		return secondOperand;
+	}
 }
